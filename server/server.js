fc59d00148c2174e8313c8103374b38010772cdb
Shalev Mualem <shalevm00@gmail.com>
2023-09-06 12:24:51 +0300

    testing

--- server/server.js
+++ server/server.js
@@ -12,10 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 
 const app=express();
-app.use(cors({
-    origin: 'http://localhost:3000',
-    // You can add more trusted origins as needed
-  }));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
