3a912ab7ba07e96ac57733f62e793d8aa26c699f
Shalev Mualem <shalevm00@gmail.com>
2023-09-06 12:50:54 +0300

    cors

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 
 const app=express();
-app.use(cors({ origin: 'https://project-amazon-client.vercel.app' }));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
